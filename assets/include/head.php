bac3daba310d0a7433ccc647a363497261c4dfbb
Nicolas Raineson <nicolasraineson@mac.home>
2025-10-30 18:10:26 +0100

    select2

--- assets/include/head.php
+++ assets/include/head.php
@@ -2,5 +2,12 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link rel="stylesheet" href="assets/css/main.css" />
 		<link rel="stylesheet" href="assets/css/icons.css" />
+
+		<!-- Bootstrap js -->
 		<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js"integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y"crossorigin="anonymous"></script>
+		
+		<!-- select2.js -->
+		<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+		<link href="assets/css/select2.css" rel="stylesheet" />
+		<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
